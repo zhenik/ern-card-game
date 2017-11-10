505670a57f29c844ca75af5cd947e592a7e89102
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-11-10 17:06:09 +0100

    update gameLogicController

--- gamelogic/src/main/kotlin/no/ern/game/gamelogic/controller/GameLogicController.kt
+++ gamelogic/src/main/kotlin/no/ern/game/gamelogic/controller/GameLogicController.kt
@@ -3,9 +3,15 @@ package no.ern.game.gamelogic.controller
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
+import io.swagger.annotations.ApiResponse
+import no.ern.game.gamelogic.domain.converters.PlayerFightConverter
 import no.ern.game.gamelogic.domain.converters.PlayerSearchConverter
+import no.ern.game.gamelogic.domain.model.Player
+import no.ern.game.gamelogic.services.GameProcessService
+import no.ern.game.schema.dto.ItemDto
 import no.ern.game.schema.dto.UserDto
 import no.ern.game.schema.dto.gamelogic.PlayerSearchDto
+import no.ern.game.schema.dto.gamelogic.PlayersFightDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
@@ -27,12 +33,15 @@ class GameLogicController {
     @Autowired
     lateinit var restTemplate : RestTemplate
 
+    @Autowired
+    lateinit var gameService: GameProcessService
+
     @Value("\${gameApis.user.path}")
     private lateinit var usersPath: String
 
     @ApiOperation("Find opponent")
     @GetMapping(path = arrayOf("/hunting"), consumes = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
-    fun createMatchResult(
+    fun findEnemy(
             @ApiParam("Search enemy for specific lvl")
             @RequestParam("level", required = false)
             level: Int?
@@ -73,5 +82,51 @@ class GameLogicController {
         }
     }
 
-}
+    @ApiOperation("Initiate match")
+    @PostMapping(path = arrayOf("/fight"),consumes = arrayOf(MediaType.APPLICATION_JSON_UTF8_VALUE))
+    @ApiResponse(code = 200, message = "The log of match")
+    fun startFight(
+            @ApiParam("Model represent ids of users who are going to fight against each other")
+            @RequestBody resultDto: PlayersFightDto) : ResponseEntity<Long>{
 
+        // 1 TODO: validate PlayersFightDto
+        // 2.1 TODO: fetch users from user-module (if any errors -> propagate them)
+        // 2.2 TODO: fetch their items (validate lvl requirements for each item)
+        // 3 call GameProcessService.fight(attacker,defender) -> return GameLogDto
+        // 4 TODO: send matchResult to MatchResult processor (if use rabbitMq || persist directly via HTTP)
+        // 5 TODO: generate experience for player(s) and persist it
+        // 6 TODO: return GameLogDto
+
+
+
+
+
+        //2.1 Users
+        val attackerUserDtoMock = UserDto("1","attackerName",null,null,100,10,null,null,1,null)
+        val defenderUserDtoMock = UserDto("2","defenderName",null,null,120,12,null,null,2,null)
+        //2.2 Items
+        val randomItems1: List<ItemDto> = getMockListOfItems()
+        val randomItems2: List<ItemDto> = getMockListOfItems()
+
+        val attacker: Player = PlayerFightConverter.transform(attackerUserDtoMock,randomItems1)
+        val defender: Player = PlayerFightConverter.transform(defenderUserDtoMock,randomItems2)
+
+
+        //3 TODO: return FightResultLogDto
+        gameService.fight(attacker,defender)
+
+        return ResponseEntity.ok(1L)
+
+    }
+
+
+    private fun getMockListOfItems():List<ItemDto>{
+        // 1 weapon
+        // 1 armor
+        return listOf(
+                ItemDto(null,null,null,(Math.random()*15).toLong(),0),
+                ItemDto(null,null,null,0,(Math.random()*15).toLong())
+                )
+    }
+
+}
