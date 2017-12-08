84754fcc0339472c0242b07272f901a50235ab0f
EirikSkogstad <skogeir15@student.westerdals.no>
2017-12-08 15:40:29 +0100

    Fixed PlayerRepository tests

--- player/src/test/kotlin/no/ern/game/player/repository/PlayerRepositoryImplTest.kt
+++ player/src/test/kotlin/no/ern/game/player/repository/PlayerRepositoryImplTest.kt
@@ -92,121 +92,129 @@ class EntityRepositoryImplTest {
         val wasSuccessful = repo.addItem(savedId, 1L)
         assertFalse(wasSuccessful)
     }
-//
-//    @Test
-//    fun testFindPlayerByLevel() {
-//        val player1 = getValidPlayers()[0]
-//        val player2 = getValidPlayers()[1]
-//        // Change level so that we find more than one.
-//        player2.level = player1.level
-//        val player3 = getValidPlayers()[2]
-//
-//        createPlayer(player1)
-//        createPlayer(player2)
-//        createPlayer(player3)
-//
-//        assertEquals(3, repo.count())
-//        val playersFound1 = repo.findAllByLevel(player1.level)
-//        val playersFound2 = repo.findAllByLevel(player3.level)
-//
-//        assertEquals(2, playersFound1.count())
-//        assertTrue(playersFound1.any({ e -> e.health == player1.health }))
-//        assertTrue(playersFound1.any({ e -> e.health == player2.health }))
-//
-//        assertEquals(1, playersFound2.count())
-//        assertTrue(playersFound2.any({ e -> e.health == player3.health }))
-//    }
-//
-//    @Test
-//    fun testUpdatePlayer() {
-//        val player1 = getValidPlayers()[0]
-//        val player2 = getValidPlayers()[1]
-//
-//        createPlayer(player1)
-//        assertEquals(1, repo.count())
-//
-//        val wasSuccessful = updatePlayer(player2, player1.id!!)
-//        assertEquals(true, wasSuccessful)
-//
-//        val readPlayer = repo.findOne(player1.id)
-//
-//        assertEquals(readPlayer?.health, player2.health)
-//        assertEquals(readPlayer?.id, player1.id)
-//        assertEquals(readPlayer?.currency, player2.currency)
-//
-//        assertEquals(1, repo.count())
-//    }
-//
-//    @Test
-//    fun testChangeIdByUpdate() {
-//        val player1 = getValidPlayers()[0]
-//        val player2 = getValidPlayers()[1]
-//
-//        createPlayer(player1)
-//
-//        updatePlayer(player2, player1.id!!)
-//
-//        val playerFound = repo.findOne(player2.id)
-//        assertNull(playerFound)
-//        assertEquals(1, repo.count())
-//    }
-//
-//    @Test
-//    fun testDeletePlayer() {
-//        val player1 = getValidPlayers()[0]
-//        val player2 = getValidPlayers()[1]
-//        createPlayer(player1)
-//        createPlayer(player2)
-//
-//        assertEquals(2, repo.count())
-//        repo.delete(player1.id)
-//
-//        assertEquals(1, repo.count())
-//
-//        repo.delete(player2.id)
-//        assertEquals(0, repo.count())
-//    }
-//
-//
-//    @Test
-//    fun testDeleteWhenNoPlayerExists() {
-//        assertEquals(0, repo.count())
-//        try {
-//            repo.delete(2323)
-//            fail("Delete id that doesnt should throw exception")
-//        } catch (e: Exception) {
-//
-//        }
-//        assertEquals(0, repo.count())
-//    }
-//
-//
-//    // Constraints
-//    @Test
-//    fun testPositiveIntegerConstraint() {
-//        val player = getValidPlayers()[0]
-//
-//        player.level = 9000
-//
-//        val wasSuccesful = createPlayer(player)
-//        assertFalse(wasSuccesful)
-//
-//        val foundPlayer = repo.findOne(player.id)
-//        assertNull(foundPlayer)
-//    }
-//
-//    @Test
-//    fun testNegativeIntegerConstraint() {
-//        val player = getValidPlayers()[0]
-//
-//        player.level = -23
-//
-//        createPlayer(player)
-//        //assertEquals(false, isSaveSuccessful)
-//
-//        val foundPlayer = repo.findOne(player.id)
-//        assertEquals(null, foundPlayer)
-//    }
+
+    @Test
+    fun testFindPlayerByLevel() {
+        val player1 = getValidPlayers()[0]
+        val player2 = getValidPlayers()[1]
+        // Change level so that we find more than one.
+        player2.level = player1.level
+        val player3 = getValidPlayers()[2]
+
+        createPlayer(player1)
+        createPlayer(player2)
+        createPlayer(player3)
+
+        assertEquals(3, repo.count())
+        val playersFound1 = repo.findAllByLevel(player1.level)
+        val playersFound2 = repo.findAllByLevel(player3.level)
+
+        assertEquals(2, playersFound1.count())
+        assertTrue(playersFound1.any({ e -> e.health == player1.health }))
+        assertTrue(playersFound1.any({ e -> e.health == player2.health }))
+
+        assertEquals(1, playersFound2.count())
+        assertTrue(playersFound2.any({ e -> e.health == player3.health }))
+    }
+
+    @Test
+    fun testUpdatePlayer() {
+        val player1 = getValidPlayers()[0]
+        val player2 = getValidPlayers()[1]
+
+        val savedId = createPlayer(player1)
+        assertEquals(1, repo.count())
+
+        val wasSuccessful = updatePlayer(player2, savedId)
+        assertEquals(true, wasSuccessful)
+
+        val readPlayer = repo.findOne(savedId)
+
+        assertEquals(readPlayer?.health, player2.health)
+        assertEquals(readPlayer?.id, savedId)
+        assertEquals(readPlayer?.currency, player2.currency)
+
+        assertEquals(1, repo.count())
+    }
+
+    @Test
+    fun testChangeIdInUpdate_ShouldFail() {
+        val player1 = getValidPlayers()[0]
+        val player2 = getValidPlayers()[1]
+
+        val savedId = createPlayer(player1)
+
+        // Try to change id in saved player
+        player2.id = savedId * 2
+        updatePlayer(player2, savedId)
+
+
+        // Validate that id did not change
+        val playerFound = repo.findOne(savedId * 2)
+        assertNull(playerFound)
+        assertEquals(1, repo.count())
+    }
+
+    @Test
+    fun testDeletePlayer() {
+        val player1 = getValidPlayers()[0]
+        val player2 = getValidPlayers()[1]
+
+        val savedId1 = createPlayer(player1)
+        val savedId2 = createPlayer(player2)
+
+        assertEquals(2, repo.count())
+        repo.delete(savedId1)
+
+        assertEquals(1, repo.count())
+
+        repo.delete(savedId2)
+        assertEquals(0, repo.count())
+    }
+
+    @Test
+    fun testDeleteWhenNoPlayerExists() {
+        assertEquals(0, repo.count())
+        try {
+            repo.delete(2323)
+            fail("Delete id that doesnt should throw exception")
+        } catch (e: Exception) {
+
+        }
+        assertEquals(0, repo.count())
+    }
+
+//                  Constraints
+
+    @Test
+    fun testPositiveIntegerConstraint() {
+        val player = getValidPlayers()[0]
+        var savedId = -1L
+
+        player.level = 9000
+        try {
+            savedId = createPlayer(player)
+            fail()
+        } catch (e: Exception) {}
+
+        // Player should not be persisted
+        assertEquals(-1, savedId)
+    }
+
+    @Test
+    fun testNegativeIntegerConstraint() {
+        val player = getValidPlayers()[0]
+        var savedId = -1L
+
+        player.level = -23
+        try {
+            savedId = createPlayer(player)
+            fail()
+        } catch (e: Exception) {}
+
+        // Player should not be persisted
+        assertEquals(-1, savedId)
+    }
 
 
     fun getValidPlayers(): List<Player> {
