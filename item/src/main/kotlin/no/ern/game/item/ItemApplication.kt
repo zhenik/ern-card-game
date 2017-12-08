0b19f33352bc29e29b7968883f3871bbeb0ca18c
RobertEikeland <eikrob15@student.westerdals.no>
2017-12-08 14:41:24 +0100

    Add Eureka and have Item register

--- item/src/main/kotlin/no/ern/game/item/ItemApplication.kt
+++ item/src/main/kotlin/no/ern/game/item/ItemApplication.kt
@@ -3,9 +3,13 @@ package no.ern.game.item
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.ribbon.RibbonClient
 
 
 @SpringBootApplication(scanBasePackages = arrayOf("no.ern.game.item"))
+@EnableEurekaClient
+@RibbonClient
 class Application : WebMvcConfigurerAdapter() {}
 
 fun main(args: Array<String>) {
