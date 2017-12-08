a225a2eade3da9dc2712fe280f8c984456bc0af3
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-12-08 20:55:41 +0100

    add eureka & docker to match module

--- match/src/main/kotlin/no/ern/game/match/MatchResultApplication.kt
+++ match/src/main/kotlin/no/ern/game/match/MatchResultApplication.kt
@@ -2,9 +2,11 @@ package no.ern.game.match
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @SpringBootApplication(scanBasePackages = arrayOf("no.ern.game.match"))
+@EnableEurekaClient
 class Application : WebMvcConfigurerAdapter() {}
 
 fun main(args: Array<String>) {
