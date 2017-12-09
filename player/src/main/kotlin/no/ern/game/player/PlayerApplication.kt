343b1ed74701d717587b2a761e57372553061603
EirikSkogstad <skogeir15@student.westerdals.no>
2017-12-09 13:25:25 +0100

    Added eureka configuration

--- player/src/main/kotlin/no/ern/game/player/PlayerApplication.kt
+++ player/src/main/kotlin/no/ern/game/player/PlayerApplication.kt
@@ -2,10 +2,14 @@ package no.ern.game.player
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.ribbon.RibbonClient
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 
 @SpringBootApplication(scanBasePackages = arrayOf("no.ern.game.player"))
+@EnableEurekaClient
+@RibbonClient(name = "player-server")
 class Application : WebMvcConfigurerAdapter() {}
 
 fun main(args: Array<String>) {
