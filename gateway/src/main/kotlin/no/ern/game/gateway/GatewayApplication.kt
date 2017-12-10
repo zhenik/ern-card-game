20830e7012794bcc478e88cbcb83dfa346afe74c
EirikSkogstad <skogeir15@student.westerdals.no>
2017-12-10 16:40:28 +0100

    Added files and folder for Gateway

---
+++ gateway/src/main/kotlin/no/ern/game/gateway/GatewayApplication.kt
@@ -0,0 +1,13 @@
+package no.ern.game.gateway
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+
+@EnableZuulProxy
+@SpringBootApplication
+class GatewayApplication {}
+
+fun main(args: Array<String>) {
+    SpringApplication.run(GatewayApplication::class.java, *args)
+}
