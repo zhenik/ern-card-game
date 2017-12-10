6981c50d7d89b84dc5ce7ba59bbcd69331dc0477
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-12-10 18:45:50 +0100

    add eureka to gamelogic

--- gamelogic/src/main/kotlin/no/ern/game/gamelogic/GameLogicApplication.kt
+++ gamelogic/src/main/kotlin/no/ern/game/gamelogic/GameLogicApplication.kt
@@ -2,10 +2,12 @@ package no.ern.game.gamelogic
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 
 @SpringBootApplication(scanBasePackages = arrayOf("no.ern.game.gamelogic"))
+@EnableEurekaClient
 class GameLogicApplication : WebMvcConfigurerAdapter() {}
 
 fun main(args: Array<String>) {
