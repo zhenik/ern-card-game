676b06d7ff5e064573a97cd527983811635c98f1
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-12-13 17:15:00 +0100

    test distributed session v.3

--- userservice/src/main/kotlin/no/ern/game/userservice/Application.kt
+++ userservice/src/main/kotlin/no/ern/game/userservice/Application.kt
@@ -2,8 +2,10 @@ package no.ern.game.userservice
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class Application
 
 
