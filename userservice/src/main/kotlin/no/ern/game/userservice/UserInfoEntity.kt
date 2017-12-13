19f310b6161af23fece9eee4119ae06efdc423ff
Nikita Zhevnitskiy <nikita.zhevnitskiy@gmail.com>
2017-12-13 15:57:57 +0100

    test distributed session

---
+++ userservice/src/main/kotlin/no/ern/game/userservice/UserInfoEntity.kt
@@ -0,0 +1,25 @@
+package no.ern.game.userservice
+
+import org.hibernate.validator.constraints.Email
+import org.hibernate.validator.constraints.NotBlank
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@Entity
+class UserInfoEntity(
+
+        @get:Id
+        @get:NotBlank
+        var userId: String?,
+
+        @get:NotBlank
+        var name: String?,
+
+        var middleName: String?,
+
+        @get:NotBlank
+        var surname: String?,
+
+        @get:Email
+        var email: String?
+)
